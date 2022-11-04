1d0afdb5267ee1fca2e3716013824f8acd4c4bab
Maciek <maciej@janowski.dev>
2022-11-04 20:39:50 -0300

    add website app

--- app/budget_api/urls.py
+++ app/budget_api/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include("website.urls")),
     path("", include("budgets.urls")),
 ]
